887cca5c2e8193b09d6cb61531059563f2dd778b
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-23 15:56:55 +0200

    2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,4 +2,5 @@
 exec { 'install_flask':
     command => '/usr/bin/pip3 install flask==2.1.0',
     path    => ['/usr/bin', '/usr/local/bin'],
+    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
