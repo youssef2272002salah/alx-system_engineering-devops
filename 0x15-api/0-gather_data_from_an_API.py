787ec744f0fa22711912dc1d655226e68e4ab4ea
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-28 02:07:12 +0200

    1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script that, using a REST API"""
+from requests import get
+from sys import argv
+import json
+
+if __name__== "__main__":
+
+    response1=get('https://jsonplaceholder.typicode.com/todos/')
+    todo_data= response1.json()
+    user_id = int(argv[1])
+
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+    name = [res for res in data2 if res["id"] == user_id]
+    name = name[0]["name"]
+
+    todo_data_filtered_before = [res for res in todo_data if res["userId"] == user_id]
+    todo_data_filtered_after = [res for res in todo_data if res["userId"] == user_id and res["completed"]]
+    
+    print (f"Employee {name} is done with tasks({len(todo_data_filtered_after)}/{len(todo_data_filtered_before)}):")
+    for i in todo_data_filtered_after:
+        print("\t "+i["title"])
+        
+    
