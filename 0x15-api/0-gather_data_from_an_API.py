8026d868ceed19c56689c3f4bfda61ec09d7c0c9
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-28 02:08:28 +0200

    1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Python script that, using a REST API"""
+import json
 from requests import get
 from sys import argv
-import json
 
-if __name__== "__main__":
 
-    response1=get('https://jsonplaceholder.typicode.com/todos/')
-    todo_data= response1.json()
+if __name__ == "__main__":
+
+    response1 = get('https://jsonplaceholder.typicode.com/todos/')
+    todo_data = response1.json()
     user_id = int(argv[1])
 
     response2 = get('https://jsonplaceholder.typicode.com/users')
@@ -15,11 +16,12 @@ if __name__== "__main__":
     name = [res for res in data2 if res["id"] == user_id]
     name = name[0]["name"]
 
-    todo_data_filtered_before = [res for res in todo_data if res["userId"] == user_id]
-    todo_data_filtered_after = [res for res in todo_data if res["userId"] == user_id and res["completed"]]
-    
-    print (f"Employee {name} is done with tasks({len(todo_data_filtered_after)}/{len(todo_data_filtered_before)}):")
+    todo_data_filtered_before = [
+        res for res in todo_data if res["userId"] == user_id]
+    todo_data_filtered_after = [
+        res for res in todo_data if res["userId"] == user_id and res["completed"]]
+
+    print(
+        f"Employee {name} is done with tasks({len(todo_data_filtered_after)}/{len(todo_data_filtered_before)}):")
     for i in todo_data_filtered_after:
-        print("\t "+i["title"])
-        
-    
+        print("\t " + i["title"])
