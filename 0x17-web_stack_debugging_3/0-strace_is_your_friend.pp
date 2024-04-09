2b0f38ef8dfec4a63d603053b807041c1cb2a858
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-10 00:06:03 +0200

    1

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Creat a manifest that fix all termintion of php.
+
+exec { 'fix_php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
