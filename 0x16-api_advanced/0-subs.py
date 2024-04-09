083713da98f1679082a38a9dabacdc26c81a9579
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-09 17:12:39 +0200

    2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,12 @@ import json
 def number_of_subscribers(subreddit):
     ''' returns number of subscribers '''
     url = 'https://www.reddit.com/r/{}/about.json'.format(
-        subreddit)  # Corrected URL
+        subreddit)
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     data = response.json()
     # data = json.dumps(data["data"], indent=4)
     # print (data)
-    if response.status_code == 404:
-        return 0
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
+    return 0
