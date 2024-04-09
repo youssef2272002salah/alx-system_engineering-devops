fddf6236864b227d69b246820bdfd551710d6e9c
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-09 17:44:14 +0200

    2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' 0-subs.py '''
+import requests
+import json
+
+
+def top_ten(subreddit):
+    ''' returns number of subscribers '''
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(
+        subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    params = {"limit": 10}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False)
+    try:
+
+        data = response.json().get("data").get("children")
+        for child in data:
+            print(child.get("data").get("title"))
+    except KeyError:
+        print("None")
