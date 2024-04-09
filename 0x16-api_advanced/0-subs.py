9120e7f8b19862322c5e29b68c77f69c6975ff8d
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-09 17:17:14 +0200

    6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ def number_of_subscribers(subreddit):
     ''' returns number of subscribers '''
     import requests
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(
-           subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if response.status_code >= 300:
         return 0
 
